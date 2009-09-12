57f649b06ba9160136639fb1f26e1a5fb0e29bdf
ccdjh.marx <yx0662@gmail.com>
2009-09-12 10:16:25 +0800

    0.010.48

--- cm/static/javascript/base.js
+++ cm/static/javascript/base.js
@@ -1,7 +1,11 @@
   $(document).ready(function(){
     $("#commentForm").validate();
+    $("#oneForm").validate();
     $("#aboutForm").validate();
     $("#listFrom").validate();
-    $("#listFromTwo").validate();
+    $(".listFromTwo").rules("add", {
+      required: true,
+      minlength: 2
+    });
     $("#adminForm").validate();
   });
